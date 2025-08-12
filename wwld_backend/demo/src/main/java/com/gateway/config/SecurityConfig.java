185e25aab5645a8d37f9a050ad5a1de8e06320ae
Tran Chien <tranminhchien.b9362@gmail.com>
2025-08-12 20:55:34 +0700

    sdas

--- wwld_backend/demo/src/main/java/com/gateway/config/SecurityConfig.java
+++ wwld_backend/demo/src/main/java/com/gateway/config/SecurityConfig.java
@@ -1,42 +1,26 @@
 package com.gateway.config;
 
-import com.gateway.service.CustomUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/admin/**").hasRole("ADMIN")  // hoặc .hasRole("ADMIN")
-//                        .anyRequest().permitAll()
-//                )
-//                .httpBasic(AbstractHttpConfigurer::disable);
-//
-//        return http.build();
-//    }
-
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http, AdminBypassFilter bypass) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .addFilterBefore(bypass, UsernamePasswordAuthenticationFilter.class)
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/admin/**").hasRole("ADMIN")  // hoặc .hasRole("ADMIN")
                         .anyRequest().permitAll()
                 )
-                .csrf(AbstractHttpConfigurer::disable); // tùy nhu cầu
+                .httpBasic(AbstractHttpConfigurer::disable);
+
         return http.build();
     }
 
